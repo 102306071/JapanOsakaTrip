5dd69ef75cad68efc38b5a529820b9d940fc434b
102306071 <102306071@nccu.edu.tw>
2016-12-08 04:00:32 +0800

    test
    test

--- javascripts/head.js
+++ javascripts/head.js
@@ -1,12 +1,13 @@
 document.writeln("    <meta http-equiv=\'X-UA-Compatible\' content=\'chrome=1\'>");
 document.writeln("    <meta name=\'description\' content=\'Japanosakatrip : \'>");
+document.writeln("    <meta name=\'viewport\' content=\'width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;\'>");
 document.writeln("");
 document.writeln("    <link rel=\'stylesheet\' type=\'text/css\' media=\'screen\' href=\'stylesheets/stylesheet.css\'>");
 document.writeln("    <link rel=\'stylesheet\' type=\'text/css\' media=\'screen\' href=\'stylesheets/bootstrap.css\'>");
-document.writeln("    <link rel=\'apple-touch-icon\' href=\'icon/touch-icon-iphone.png\' />");
-document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'76x76\' href=\'icon/touch-icon-ipad.png\' />");
-document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'120x120\' href=\'icon/touch-icon-iphone-retina.png\' />");
-document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'152x152\' href=\'icon/touch-icon-ipad-retina.png\' />");
+document.writeln("    <link rel=\'apple-touch-icon\' href=\'touch-icon-iphone.png\' />");
+document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'76x76\' href=\'touch-icon-ipad.png\' />");
+document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'120x120\' href=\'touch-icon-iphone-retina.png\' />");
+document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'152x152\' href=\'touch-icon-ipad-retina.png\' />");
 document.writeln("    <!-- jQuery函式庫 -->");
 document.writeln("    <script src=\'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\'></script>");
 document.writeln("    <!-- Bootstrap的js函式庫 -->");
