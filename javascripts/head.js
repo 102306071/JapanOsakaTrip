afd393c67b60828d4cf6572873f27037b6b852d5
102306071 <102306071@nccu.edu.tw>
2016-12-08 03:43:15 +0800

    add icon
    add icon
    edit day6 time

--- javascripts/head.js
+++ javascripts/head.js
@@ -4,6 +4,10 @@ document.writeln("    <meta name=\'viewport\' content=\'width=device-width; init
 document.writeln("");
 document.writeln("    <link rel=\'stylesheet\' type=\'text/css\' media=\'screen\' href=\'stylesheets/stylesheet.css\'>");
 document.writeln("    <link rel=\'stylesheet\' type=\'text/css\' media=\'screen\' href=\'stylesheets/bootstrap.css\'>");
+document.writeln("    <link rel=\'apple-touch-icon\' href=\'icon/touch-icon-iphone.png\' />");
+document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'76x76\' href=\'icon/touch-icon-ipad.png\' />");
+document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'120x120\' href=\'icon/touch-icon-iphone-retina.png\' />");
+document.writeln("    <link rel=\'apple-touch-icon\' sizes=\'152x152\' href=\'icon/touch-icon-ipad-retina.png\' />");
 document.writeln("    <!-- jQuery函式庫 -->");
 document.writeln("    <script src=\'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\'></script>");
 document.writeln("    <!-- Bootstrap的js函式庫 -->");
